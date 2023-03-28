e84cc09e83054594c97565bb2e14141ec00f4df9
theirish81 <theirish81@gmail.com>
2023-03-28 11:02:46 +0200

    documentation

--- auth/users.go
+++ auth/users.go
@@ -1,13 +1,16 @@
 package auth
 
 import (
+	"os"
+
 	"golang.org/x/crypto/bcrypt"
 	"gopkg.in/yaml.v3"
-	"os"
 )
 
+// Users is a map of User where the key is the username, and the value represent the details
 type Users map[string]User
 
+// Authenticate will return a user if username and password match our users file
 func (u Users) Authenticate(username string, password string) *User {
 	if ux, ok := u[username]; ok && bcrypt.CompareHashAndPassword([]byte(ux.Password), []byte(password)) == nil {
 		return &ux
@@ -15,12 +18,14 @@ func (u Users) Authenticate(username string, password string) *User {
 	return nil
 }
 
+// User is one user configuration
 type User struct {
 	Password  string   `yaml:"password"`
 	Access    string   `yaml:"access"`
 	Sequences []string `yaml:"sequences"`
 }
 
+// LoadUsers loads the users from file
 func LoadUsers(filePath string) (Users, error) {
 	data, err := os.ReadFile(filePath)
 	if err != nil {
--- dto/v1/errors.go
+++ dto/v1/errors.go
@@ -1,5 +1,6 @@
 package v1
 
+// Error is the error DTO
 type Error struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
