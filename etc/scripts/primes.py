8750ba48bd55d8530564f0c9a60c0e84ef86cdc5
theirish81 <theirish81@gmail.com>
2024-07-21 16:53:34 +0200

    normalizing cases

--- etc/scripts/primes.py
+++ etc/scripts/primes.py
@@ -7,7 +7,7 @@ for line in sys.stdin:
     data = data+line
 
 parsed_data = json.loads(data)
-num = parsed_data["Params"]["number"]
+num = parsed_data["params"]["number"]
 flag = False
 if num > 1:
     for i in range(2, num):
